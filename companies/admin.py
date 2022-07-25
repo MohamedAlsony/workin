0e78b7ed75b5daa21a1f116e93ce05be84649bb8
MohamedAlsony <mohamedahmedelsony@gmail.com>
2022-07-25 03:11:00 +0200

    '#'

--- companies/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from companies.models import CompanyProfile,Review
-#edit the model name
-# Register your models here.
-admin.site.register(CompanyProfile)
-admin.site.register(Review)
-
-# Register your models here.
--- jobs/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from jobs.models import Jobs, Applicant
-# Register your models here.
-admin.site.register(Jobs)
-admin.site.register(Applicant)
